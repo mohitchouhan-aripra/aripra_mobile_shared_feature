679520bf8441ee631b862883b5206700c4dddda3
Mohit chouhan Aripra <mohit.chouhan@aripratech.com>
2026-01-08 20:13:15 +0530

    Update custom_load_more.dart

--- lib/pagination_widget/custom_load_more.dart
+++ lib/pagination_widget/custom_load_more.dart
@@ -65,7 +65,7 @@ class _CustomLoadMoreState extends State<CustomLoadMore> {
       return RefreshIndicator(
         onRefresh: () async {
           page=0;
-          await widget.onLoadMore?.call(page),
+          await widget.onLoadMore?.call(page);
           }
         child: child,
       );
